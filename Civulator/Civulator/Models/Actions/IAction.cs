d0aaa77498ab0f411fdc9bdf08e2273a46eed734
Sylvia Olliff <sylviaolliff@gmail.com>
2025-03-06 21:18:19 -0500

    Minor update to IAction

--- Civulator/Civulator/Models/Actions/IAction.cs
+++ Civulator/Civulator/Models/Actions/IAction.cs
@@ -1,12 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Civulator.Models.Conditions;
 
-namespace Civulator.Models.Actions
+namespace Civulator.Models.Actions;
+
+public interface IAction
 {
-    internal interface IAction
-    {
-    }
+    public string Name { get; }
+    public int Duration { get; }
+    public List<ICondition> PossibleConditions { get; }
+    public List<ICondition> RequiredConditions { get; }
+
+    public ActionResult ActionResult { get; }
 }
